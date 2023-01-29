5625f81cfcc78870cba76956fc069a4e53703040
Longin_Imperial <longin.le18@imperial.ac.uk>
2023-01-29 21:36:04 +0100

    Added table and selection based on text

--- twitter_analysis_tool/app/app.py
+++ twitter_analysis_tool/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import json
 #from /src/data/helper_functions import get_tweets
 
 from flask import Flask
@@ -7,19 +8,22 @@ app = Flask(__name__)
 app.debug = True
 
 
-#pd.read_csv()
-charts = ['chart1', 'chart2', 'chart3']
-
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    input = None
+    tweets_from_json = pd.read_json(f'../data/processed/CryptoTubylec-tweets.json')[['created_at','id','text','retweet_count','favorite_count']]
+
     if request.method == 'POST':
-        input = request.form["inpstring"]
-    else:
-        chart_name = None
-    return render_template('index.html', inpstring=input, chart_name=charts)
+        #chart_type = request.form.get('chart')
+        #input = request.form["inpstring"]
+        
+        string = request.form["inpstring"].lower()
+        filtered_df = tweets_from_json[tweets_from_json['text'].str.lower().str.contains(string, na=False)]
+        
+        return render_template("index.html", inpstring=string, table=filtered_df.to_html())
+    
+    return render_template('index.html', inpstring=string, table=tweets_from_json.to_html())
 
 if __name__ == "__main__":
     app.run(debug=True)
